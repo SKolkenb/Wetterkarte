34f7925320139286c4c3fb9f5a2382adc8793a94
SKolkenb <143170876+SKolkenb@users.noreply.github.com>
2023-10-01 16:05:05 +0200

    Update layers.js

--- layers/layers.js
+++ layers/layers.js
@@ -99,7 +99,7 @@ var lyr_Autowarn_Vorhersage_6 = new ol.layer.Tile({
                           });
               wms_layers.push([lyr_Autowarn_Vorhersage_6, 1]);
 
-lyr_GoogleMaps_0.setVisible(true);lyr_WarngebietedesDWDaufKreisbasis_1.setVisible(false);lyr_WetterwarnungenundVorabinformationenaufLandkreisebene_2.setVisible(true);lyr_Niederschlagsradar_3.setVisible(true);lyr_Autowarn_Analyse_5.setVisible(true);lyr_Autowarn_Vorhersage_6.setVisible(true);
+lyr_GoogleMaps_0.setVisible(true);lyr_EDP_0.setVisible(false);lyr_WarngebietedesDWDaufKreisbasis_1.setVisible(false);lyr_WetterwarnungenundVorabinformationenaufLandkreisebene_2.setVisible(true);lyr_Niederschlagsradar_3.setVisible(true);lyr_Autowarn_Analyse_5.setVisible(true);lyr_Autowarn_Vorhersage_6.setVisible(true);
 var layersList = [lyr_GoogleMaps_0,lyr_EDP_0,lyr_WarngebietedesDWDaufKreisbasis_1,lyr_WetterwarnungenundVorabinformationenaufLandkreisebene_2,lyr_Niederschlagsradar_3,lyr_Autowarn_Analyse_5,lyr_Autowarn_Vorhersage_6];
 setInterval(function() {
         lyr_Niederschlagsradar_3.setVisible(false);
